2983877dbe3a26f3b238f7fe19da82915996f0b6
admin <505316314@qq.com>
2022-03-19 17:46:01 +0800

    yulan

---
+++ precache-manifest.3de6aade2bcd37538c0eebc495e65cf4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bbbd659d0feac08a1a2009ff386902f",
+    "url": "/MyAccount/index.html"
+  },
+  {
+    "revision": "3cfc7689ab27f8c899ef",
+    "url": "/MyAccount/static/css/main.47f2af02.chunk.css"
+  },
+  {
+    "revision": "5da83fdef1837b8530a2",
+    "url": "/MyAccount/static/js/2.2922cbd4.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/MyAccount/static/js/2.2922cbd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cfc7689ab27f8c899ef",
+    "url": "/MyAccount/static/js/main.e48c4dc4.chunk.js"
+  },
+  {
+    "revision": "0e28277b751f9de4bda9",
+    "url": "/MyAccount/static/js/runtime-main.818ab7ea.js"
+  }
+]);
