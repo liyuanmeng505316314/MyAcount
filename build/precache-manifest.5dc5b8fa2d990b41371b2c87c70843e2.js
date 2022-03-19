a5febf8d21854856ef6d074cc4d90a31b1e5f176
admin <505316314@qq.com>
2022-03-19 12:36:39 +0800

    a

---
+++ build/precache-manifest.5dc5b8fa2d990b41371b2c87c70843e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62643f001f797dc3afea3115b943fe09",
+    "url": "/morney-13-website/index.html"
+  },
+  {
+    "revision": "c1a19fb32ea20afa4f85",
+    "url": "/morney-13-website/static/css/main.47f2af02.chunk.css"
+  },
+  {
+    "revision": "fa0909f9d293c7eb250a",
+    "url": "/morney-13-website/static/js/2.7052fe00.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/morney-13-website/static/js/2.7052fe00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1a19fb32ea20afa4f85",
+    "url": "/morney-13-website/static/js/main.07b78796.chunk.js"
+  },
+  {
+    "revision": "e11a2d36a779c4ce567a",
+    "url": "/morney-13-website/static/js/runtime-main.1261f4d6.js"
+  }
+]);
